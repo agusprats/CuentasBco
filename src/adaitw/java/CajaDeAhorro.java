95f1b4adbf170fc8db71731c8aa6e3bd9b0a416d
Agustina Prats <agusprats@gmail.com>
2021-01-19 22:00:48 -0300

    Override caja ahorros

--- src/adaitw/java/CajaDeAhorro.java
+++ src/adaitw/java/CajaDeAhorro.java
@@ -5,4 +5,12 @@ public class CajaDeAhorro extends Cuenta{
     public CajaDeAhorro(Double saldoInicial) {
         super(saldoInicial);
     }
+
+    @Override
+    public void extraer(Double monto) {
+        if(monto<= saldo){
+            super.extraer(monto);
+        }
+
+    }
 }
